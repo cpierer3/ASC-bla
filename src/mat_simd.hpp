6265c6e53e69f6f03bbd38cf232c4e2ede81be1d
jgastecker <jonas.gastecker@gmail.com>
2025-11-26 18:28:22 +0100

    parallelizing

--- src/mat_simd.hpp
+++ src/mat_simd.hpp
@@ -3,16 +3,19 @@
 
 #include <matrixview.hpp>
 #include "../myASC-HPC/src/simd.hpp"
+#include "../myASC-HPC/src/taskmanager.hpp"
 
 namespace ASC_bla {
   template<typename T>
-  Matrix<T, ORDERING::RowMajor> Multi(const MatrixView<T, ORDERING::RowMajor> A, const MatrixView<T, ORDERING::RowMajor> B) {
+  Matrix<T, ORDERING::RowMajor>
+  Multi(const MatrixView<T, ORDERING::RowMajor> A, const MatrixView<T, ORDERING::RowMajor> B) {
     Matrix<T, ORDERING::RowMajor> C(A.rows(), B.cols());
 
+    constexpr int COLS = 16;
+    constexpr int ROWS = 8;
 
-    constexpr int COLS = 8;
-    constexpr int ROWS = 4;
-    for (size_t i = 0; i < A.rows(); i += ROWS) {
+    ASC_HPC::RunParallel(A.rows() / ROWS, [&C, &B, &A](int idx, int size) {
+      const int i = idx * ROWS;
       for (size_t j = 0; j < B.cols(); j += COLS) {
 
         ASC_HPC::SIMD<double, COLS> a[ROWS];
@@ -30,28 +33,10 @@ namespace ASC_bla {
           a[t].store(&C.data()[(i + t) * C.dist() + j]);
         }
       }
-    }
+    });
     return C;
   }
 
-  template<typename T>
-  // naive testing for time difference
-  Matrix <T, ORDERING::RowMajor> Multi(const MatrixView<T, ORDERING::ColMajor> A, const MatrixView<T, ORDERING::RowMajor> B) {
-    //std::cout << "Using transpose multiplication!" << std::endl;
-    return Multi(A.transpose(), B);
 }
 
-template<typename T>
-  Matrix <T, ORDERING::RowMajor> Multi(const MatrixView<T, ORDERING::RowMajor> A, const MatrixView<T, ORDERING::ColMajor> B) {
-    //std::cout << "Using transpose multiplication!" << std::endl;
-    return Multi(A, B.transpose());
-}
-
-template<typename T>
-  Matrix <T, ORDERING::RowMajor> Multi(const MatrixView<T, ORDERING::ColMajor> A, const MatrixView<T, ORDERING::ColMajor> B) {
-    //std::cout << "Using transpose multiplication!" << std::endl;
-    return Multi(A.transpose(), B.transpose());
-
-}
-}
 #endif
